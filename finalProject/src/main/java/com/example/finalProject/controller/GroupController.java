2fc9ea4893c0d68ff04b5893f5226a2c27a631b4
SalahAlawneh <salah.alawneh@yahoo.com>
2021-05-03 23:06:29 +0300

    finish task from GroupsController

--- finalProject/src/main/java/com/example/finalProject/controller/GroupController.java
+++ finalProject/src/main/java/com/example/finalProject/controller/GroupController.java
@@ -1,6 +1,7 @@
 package com.example.finalProject.controller;
 
 import com.example.finalProject.entity.AppUser;
+import com.example.finalProject.entity.Event;
 import com.example.finalProject.entity.Groups;
 import com.example.finalProject.entity.Post;
 import com.example.finalProject.repository.GroupRepository;
@@ -9,12 +10,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
+import java.util.Set;
 
 @RestController
 public class GroupController {
@@ -38,4 +41,21 @@ public class GroupController {
         }
             return new ResponseEntity(group, HttpStatus.OK);
     };
+
+    @GetMapping("/usergroups")
+    public ResponseEntity handleGroupsFromUser() {
+        try {
+            if ((SecurityContextHolder.getContext().getAuthentication()) != null) {
+                AppUser userDetails = userRepository.findByUserName(SecurityContextHolder.getContext().getAuthentication().getName());
+                if (userDetails.getGroups() != null) {
+                    Set<Groups> userGroups = userDetails.getGroups();
+                    return new ResponseEntity(userGroups, HttpStatus.OK);
+                }
+            }
+            return new ResponseEntity(HttpStatus.OK);
+
+        } catch (Exception ex) {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+    }
 }
